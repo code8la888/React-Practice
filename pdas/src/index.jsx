1a76bfb894a7bd4cbc6bae5aa0ef39fb61538843
code8la888 <kazuhaha1997@gmail.com>
2024-12-17 21:12:55 +0800

    upload pdas and animal files

---
+++ pdas/src/index.jsx
@@ -0,0 +1,13 @@
+// 1) Import the React and ReactDom libraries;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// 2) Get a reference to the div with ID　root
+const el = document.getElementById("root");
+
+// 3) Tell React to take control of the element
+const root = ReactDOM.createRoot(el);
+
+// 4) Show the component on the screen
+root.render(<App />);
