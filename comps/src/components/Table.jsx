235ab61d9adafc815784b9647a7fc6c408a0f302
code8la888 <kazuhaha1997@gmail.com>
2024-12-26 23:13:32 +0800

    feat(table): dynamically render table rows using map function in React

--- comps/src/components/Table.jsx
+++ comps/src/components/Table.jsx
@@ -1,5 +1,26 @@
 function Table({ data }) {
-  return <div>{data.length}</div>;
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <th>{fruit.name}</th>
+        <th>{fruit.color}</th>
+        <th>{fruit.score}</th>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
