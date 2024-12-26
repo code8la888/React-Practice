3ba4561b6318c3939b434205cb21ecead74f7edf
code8la888 <kazuhaha1997@gmail.com>
2024-12-26 23:25:17 +0800

    feat(table): enhance table styling and add color square for color column

--- comps/src/pages/TablePage.jsx
+++ comps/src/pages/TablePage.jsx
@@ -2,10 +2,10 @@ import Table from "../components/Table";
 
 function TablePage() {
   const data = [
-    { name: "Orange", color: "bg-orange-5", score: 5 },
-    { name: "Orange", color: "bg-orange-5", score: 5 },
-    { name: "Orange", color: "bg-orange-5", score: 5 },
-    { name: "Orange", color: "bg-orange-5", score: 5 },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Aple", color: "bg-red-500", score: 5 },
+    { name: "Banana", color: "bg-yellow-500", score: 5 },
+    { name: "Lime", color: "bg-green-500", score: 5 },
   ];
 
   return (
