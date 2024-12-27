67ba5ba9a7cb05a76fcfe215bd1fe20aede2c950
code8la888 <kazuhaha1997@gmail.com>
2024-12-27 22:08:44 +0800

    refactor: extract sorting logic into reusable useSort hook

--- comps/src/components/SortableTable.jsx
+++ comps/src/components/SortableTable.jsx
@@ -1,39 +1,25 @@
-import { useState } from "react";
-import Table from "./Table";
 import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
-function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-  const { config, data } = props;
+import Table from "./Table";
+import useSort from "../hooks/use-sort";
 
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+function SortableTable(props) {
+  const { config, data } = props;
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(
+    data,
+    config
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
     }
+
     return {
       ...column,
       header: () => (
         <th
           className="cursor-pointer hover:bg-gray-100"
-          onClick={() => handleClick(column.label)}
+          onClick={() => setSortColumn(column.label)}
         >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
@@ -43,23 +29,6 @@ function SortableTable(props) {
       ),
     };
   });
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   return (
     <div>
--- comps/src/components/Table.jsx
+++ comps/src/components/Table.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Fragment } from "react";
 
 function Table({ data, config, keyFn }) {
