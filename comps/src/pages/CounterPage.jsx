2ac25faff73b6c3db7ae1bbae7934aa8455eb5e8
code8la888 <kazuhaha1997@gmail.com>
2024-12-28 10:14:59 +0800

    feat: refactor reducer using Immer for state immutability

--- comps/src/pages/CounterPage.jsx
+++ comps/src/pages/CounterPage.jsx
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -9,27 +10,18 @@ const SUBMIt_FORM = "submit_form";
 const reducer = (state, action) => {
   switch (action.type) {
     case ICREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count - 1;
+      return;
     case SUBMIt_FORM:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
-
+      state.valueToAdd = action.payload;
+      return;
     default:
       return state;
   }
@@ -38,7 +30,7 @@ const reducer = (state, action) => {
 function CounterPage({ initialCount }) {
   //   const [count, setCount] = useState(initialCount);
   //   const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
